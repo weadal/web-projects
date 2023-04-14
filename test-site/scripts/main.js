a5429c964ed4e2f1e7cde797c0eaf569452dbba5
weadal <weadalfive@gmail.com>
2023-04-15 01:49:51 +0900

    test-site一旦完成

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,40 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/img-html-1.png") {
+        myImage.setAttribute("src", "images/img-html-0.png");
+    } else {
+        myImage.setAttribute("src", "images/img-html-1.png");
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `ぷにぷに選手権への準備ができたようだな、${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
+
+function setUserName() {
+    const myName = prompt("あなたの名前は何ですか");
+
+
+    if (!myName) {
+        setUserName();
+    } else {
+
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `ぷにぷに選手権への準備ができたようだな、${myName}`;
+    }
+
+}
